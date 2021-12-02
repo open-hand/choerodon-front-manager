0f188037f58e92711d9e6f6be1f794ee8d468b49
maox <xm1440606925@126.com>
2021-12-02 10:09:48 +0800

    [ADD]菜单统计多语言

---
+++ react/src/app/iam/locale/zh_CN/site-statistics.js
@@ -0,0 +1,9 @@
+const sitestatics={
+    'c7n.site-statistics.export':'导出表格csv文件',
+    'c7n.site-statistics.menu.count': '菜单点击统计',
+    'c7n.site-statistics.belong': '所属层级',
+    'c7n.site-statistics.table.name': '菜单名称',
+    'c7n.site-statistics.table.code': '菜单编码',
+    'c7n.site-statistics.table.click-sum': '菜单点击总数',
+}
+export {sitestatics};
